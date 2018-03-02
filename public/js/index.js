92b8242a63e5179e2726f484518534b7112a22bb
ammy203 <33599273+ammy203@users.noreply.github.com>
2018-03-02 15:49:25 +0500

    adding geo location functionality

--- public/js/index.js
+++ public/js/index.js
@@ -26,3 +26,27 @@ jQuery('#message-form').on('submit', function (e) {
 
   });
 });
+socket.on('newLocationMessage',function(location){
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My Current Location</a>');
+  a.attr('href',location.url);
+  li.text(`${location.from} :`);
+  li.append(a);
+  jQuery('#messages').append(li);
+
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    alert('Sorry You Don`t Have This Serves');
+  };
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    },function(){
+      console.log('Unable to fetch your location');
+    })
+  })
+})
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const { generateMessage,generateLocationMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -29,7 +29,9 @@ io.on('connection', (socket) => {
     //   createdAt: new Date().getTime()
     // });
   });
-
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  })
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
